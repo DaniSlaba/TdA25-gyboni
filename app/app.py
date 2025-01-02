2fa70722cca96c749d7523efd9f21ee4ffc1a3be
Daniela Slabá <daniela@slabi.cz>
2025-01-02 18:16:39 +0100

    Přidávám flask_restful a Resource Games

--- app/app.py
+++ app/app.py
@@ -1,13 +1,19 @@
 import os
 
 from flask import Flask, jsonify
+from flask_restful import Resource, Api 
+
 from . import db
 
+
 app = Flask(__name__)
+api = Api(app, prefix='/api/v1')
+
 
 app.config.from_mapping(
     DATABASE=os.path.join(app.instance_path, 'tourdeflask.sqlite'),
 )
+
 
 # ensure the instance folder exists
 try:
@@ -22,9 +28,19 @@ def hello_world():  # put application's code here
     return "Hello TdA!"
 
 @app.route('/api')
-def api():
+def route_api():
     data = {'organization': 'Student Cyber Games'}
     return jsonify(data)
+
+
+class Games(Resource):
+
+    def get(self):
+        return jsonify({'hry': 'všecky'})
+
+
+api.add_resource(Games, '/games')
+
 
 if __name__ == '__main__':
     app.run()
